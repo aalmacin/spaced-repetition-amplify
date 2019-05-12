5997d58930b029a91bf4b3190de77d4f03fa0e3a
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-05-12 12:31:23 -0400

    Added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { TopicsComponent } from './topics/topics.component';
+import { CardsComponent } from './cards/cards.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'topics',
+    component: TopicsComponent
+  },
+  {
+    path: 'cards/:topicid',
+    component: CardsComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
