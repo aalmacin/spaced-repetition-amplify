f58ce6ae9ffb102df918c7e636e585a681b97e12
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-05-12 18:32:15 -0400

    Added cognito and auth service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AuthService } from './auth.service';
+import { User } from './user';
+import { AmplifyService } from 'aws-amplify-angular';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'spaced-repetition-amplify';
+  user: User;
+
+  constructor(private authService: AuthService, private amplifyService: AmplifyService) {
+    this.authService.getAuth().subscribe(a => {
+      console.log(a);
+    })
+
+
+    this.amplifyService.authStateChange$.subscribe(authState => {
+      console.log(authState.user);
+    });
+  }
 }
---
+++ src/app/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { AmplifyService } from 'aws-amplify-angular';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from './user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private amplifyService: AmplifyService) {}
+
+  getAuth(): Observable<User | null> {
+    return this.amplifyService.authStateChange$.pipe(
+      map(authState =>
+        (authState.state === 'signedIn') ? authState.user : null
+      )
+    );
+  }
+}
