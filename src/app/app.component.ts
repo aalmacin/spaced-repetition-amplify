4679eead9265f0874acc539560a8665a42a651d1
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-05-13 18:29:38 -0400

    Moved auth to appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component } from "@angular/core";
-import { AuthService } from "./auth.service";
+import { Component } from '@angular/core';
+import { AmplifyService } from 'aws-amplify-angular';
+import { User } from './user';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  signedIn = false;
-  constructor(public authService: AuthService) {
-    authService.isSignedIn().subscribe(signedIn => {
-      this.signedIn = signedIn;
+  user: User;
+  signedIn: boolean;
+
+  constructor(private amplifyService: AmplifyService) {
+    this.amplifyService.authStateChange$.subscribe(authState => {
+      this.signedIn = authState.state === 'signedIn';
+      if (!authState.user) {
+        this.user = null;
+      } else {
+        this.user = authState.user;
+      }
     });
   }
 }
