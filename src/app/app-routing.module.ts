384acb3a4b7903012de64ba169b6c0242a7f1c5b
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-07-14 15:46:43 -0400

    Moved design tokens to a new page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
+import { DesignTokensComponent } from './design-tokens/design-tokens.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'design-tokens',
+    component: DesignTokensComponent
   },
   // {
   //   path: 'main',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
+import { DesignTokensComponent } from './design-tokens/design-tokens.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NotFoundComponent,
+    DesignTokensComponent,
   ],
   imports: [
     BrowserModule,
