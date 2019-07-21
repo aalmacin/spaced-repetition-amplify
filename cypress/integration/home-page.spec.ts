9af499b7939163046c4973e35ba57f0deeca7909
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-07-20 21:22:37 -0400

    Added test for home page

--- cypress/integration/home-page.spec.ts
+++ cypress/integration/home-page.spec.ts
@@ -7,5 +7,16 @@ context('Home Page', () => {
 
   it('loaded', () => {
     cy.get('app-landing-page').contains('Spaced Repetition');
+    cy.get('.nav-displayed__login').should('be.visible');
+    cy.get('.nav-list-container').should('not.be.visible');
+
+    cy.get('.banner-cta__button').contains('Start now');
+
+    cy.get('.nav-displayed__toggle-label').click();
+    cy.get('.nav-list-container').should('be.visible');
+
+    cy.get('.nav-list-close').should('be.visible');
+    cy.get('.nav-list-close').click();
+    cy.get('.nav-list-close').should('not.be.visible');
   });
 });
