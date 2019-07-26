2055ad145697e3cb75fc4eb96e0a6884aba27663
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-07-25 20:10:10 -0400

    Added ability to study all flash cards

--- src/app/main/pages/study/study.component.ts
+++ src/app/main/pages/study/study.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CardService } from '@spaced-repetition/amplify/card.service';
 import { Card } from 'src/app/types/card';
 import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { map, switchMap } from 'rxjs/operators';
+import { shuffle } from 'lodash';
 
 @Component({
   selector: 'app-study',
@@ -14,12 +17,23 @@ export class StudyComponent implements OnDestroy {
   loading = true;
   errors: string[] = [];
 
-  constructor(private cardService: CardService) {
+  constructor(private cardService: CardService, private activatedRoute: ActivatedRoute) {
     this.subscriptions.add(
-      this.cardService.getAllStudyCards().subscribe(cards => {
-        this.loading = false;
-        this.cards = cards;
-      })
+      this.activatedRoute.queryParams
+        .pipe(
+          map(q => q.topicId),
+          switchMap(topicId => {
+            if (topicId) {
+              return this.cardService.getCardsByTopicId(topicId);
+            } else {
+              return this.cardService.getAllStudyCards();
+            }
+          })
+        )
+        .subscribe(cards => {
+          this.loading = false;
+          this.cards = shuffle(cards);
+        })
     );
   }
 
