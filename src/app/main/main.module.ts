c06a04b64f01b09c505d172a4328d5c2ba45284b
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-12-20 21:34:23 -0500

    Added saved step

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -19,9 +19,17 @@ import { TopicsComponent } from './pages/topics/topics.component';
 import { CardManagerComponent } from './pages/card-manager/card-manager.component';
 
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FlashCardComponent } from './pages/study/flash-card/flash-card.component';
 
 @NgModule({
-  declarations: [MainComponent, HomeComponent, StudyComponent, TopicsComponent, CardManagerComponent],
+  declarations: [
+    MainComponent,
+    HomeComponent,
+    StudyComponent,
+    TopicsComponent,
+    CardManagerComponent,
+    FlashCardComponent
+  ],
   imports: [AuthModule, CommonModule, FormsModule, ReactiveFormsModule, MainRoutingModule, SharedModule]
 })
 export class MainModule {}
--- src/app/main/pages/study/study.component.ts
+++ src/app/main/pages/study/study.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, AfterViewInit, AfterViewChecked } from '@angular/core';
 import { CardService } from '@spaced-repetition/amplify/card.service';
 import { Card } from 'src/app/types/card';
 import { Subscription } from 'rxjs';
@@ -6,13 +6,13 @@ import { ActivatedRoute } from '@angular/router';
 import { map, switchMap, tap } from 'rxjs/operators';
 import { shuffle } from 'lodash';
 
-enum CardResult {
+export enum CardResult {
   EASY = 'easy',
   HARD = 'hard',
   PENDING = 'pending'
 }
 
-interface CardVM extends Card {
+export interface CardVM extends Card {
   result: CardResult;
 }
 
@@ -22,15 +22,10 @@ interface CardVM extends Card {
   styleUrls: ['./study.component.scss']
 })
 export class StudyComponent implements OnDestroy {
+  loading = true;
   cards: CardVM[] = [];
   subscriptions = new Subscription();
-  loading = true;
-  errors: string[] = [];
   scheduledStudy = false;
-  currentCardIndex = 0;
-  cardFrontIndex = 0;
-  cardBackIndex = 0;
-  isShow = false;
 
   constructor(private cardService: CardService, private activatedRoute: ActivatedRoute) {
     this.subscriptions.add(
@@ -53,81 +48,5 @@ export class StudyComponent implements OnDestroy {
 
   ngOnDestroy() {
     this.subscriptions.unsubscribe();
-  }
-
-  public easierCard(card: Card, i: number) {
-    if (this.scheduledStudy) {
-      this.subscriptions.add(
-        this.cardService.updateCardToEasy(card).subscribe((result: any) => {
-          if (result.error) {
-            this.errors = [result.error];
-          }
-        })
-      );
-    } else {
-      this.updateCardResults(i, CardResult.EASY);
-      if (i === 0) {
-        this.updateHarderCards();
-      }
-    }
-  }
-
-  public harderCard(card: Card, i: number) {
-    if (this.scheduledStudy) {
-      this.subscriptions.add(
-        this.cardService.updateCardToHard(card).subscribe((result: any) => {
-          if (result.error) {
-            this.errors = [result.error];
-          }
-        })
-      );
-      this.cardService.updateCardToHard(card);
-    } else {
-      this.updateCardResults(i, CardResult.HARD);
-      if (i === 0) {
-        this.updateHarderCards();
-      }
-    }
-  }
-
-  public showAnswer() {
-    console.log('show answer');
-    this.isShow = true;
-    if (this.currentCardIndex !== 0) {
-      this.cardBackIndex = this.cardBackIndex + 1;
-    }
-  }
-
-  get indexInRange() {
-    return this.currentCardIndex + 1 < this.cards.length;
-  }
-
-  get frontToShow() {
-    return this.cards[this.cardFrontIndex];
-  }
-
-  get backToShow() {
-    return this.cards[this.cardBackIndex];
-  }
-
-  public showNext() {
-    this.isShow = false;
-    this.cardFrontIndex = this.cardFrontIndex + 1;
-    if (this.indexInRange) {
-      this.currentCardIndex++;
-    }
-  }
-
-  private updateCardResults(i, cardResult: CardResult) {
-    this.cards[i].result = cardResult;
-  }
-
-  private updateHarderCards() {
-    const harderCards = this.cards.filter(c => c.result === CardResult.HARD);
-    this.cards = shuffle(harderCards).map(a => ({ ...a, result: CardResult.PENDING }));
-  }
-
-  get currentCard() {
-    return this.cards[this.currentCardIndex];
   }
 }
