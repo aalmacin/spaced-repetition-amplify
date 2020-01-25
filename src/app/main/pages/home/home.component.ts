bf5d2d62160671f7d487fdb6388eac35f0b6080d
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2020-01-25 12:42:29 -0500

    Removed stats

--- src/app/main/pages/home/home.component.ts
+++ src/app/main/pages/home/home.component.ts
@@ -1,7 +1,17 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '@spaced-repetition/amplify/auth.service';
-import { Subscription } from 'rxjs';
+import { Subscription, combineLatest } from 'rxjs';
 import { User } from '@spaced-repetition/types/user';
+import { TopicService } from '@spaced-repetition/amplify/topic.service';
+import { Topic } from '@spaced-repetition/types/topic';
+import { CardService } from '@spaced-repetition/amplify/card.service';
+import { Card } from '@spaced-repetition/types/card';
+import {
+  getTimestampFromDate,
+  getCurrentTimestamp,
+  getCurrentDate,
+  getDateFormat
+} from '@spaced-repetition/main/shared/timestamp.func';
 
 @Component({
   selector: 'app-home',
@@ -12,10 +22,40 @@ export class HomeComponent implements OnDestroy {
   subscriptions = new Subscription();
   user: User;
   createTopicToggle = false;
-  constructor(public authService: AuthService) {
+  loading = true;
+  topics: Topic[] = [];
+  cards: Card[] = [];
+  studyCards: Card[] = [];
+  nextStudy = '';
+
+  constructor(public authService: AuthService, public topicService: TopicService, public cardService: CardService) {
     this.subscriptions.add(
-      this.authService.getCurrentUser().subscribe(user => {
+      combineLatest(
+        this.authService.getCurrentUser(),
+        this.topicService.getTopics(),
+        this.cardService.getAllCards()
+      ).subscribe(([user, topics, cards]) => {
         this.user = user;
+        this.loading = false;
+        this.topics = topics;
+        this.cards = cards;
+        this.studyCards = this.cards
+          .filter(card => card.isReadyToStudy)
+          .sort((a, b) => {
+            return (
+              getTimestampFromDate(getDateFormat(b.nextStudyDate)) -
+              getTimestampFromDate(getDateFormat(a.nextStudyDate))
+            );
+          });
+        if (this.studyCards.length > 0) {
+          const nextStudyCard = this.studyCards[0];
+          const nextStudyTimestamp = getTimestampFromDate(nextStudyCard.nextStudyDate);
+          if (nextStudyTimestamp <= getCurrentTimestamp()) {
+            this.nextStudy = getCurrentDate();
+          } else {
+            this.nextStudy = getDateFormat(nextStudyCard.nextStudyDate);
+          }
+        }
       })
     );
   }
--- src/app/main/shared/timestamp.func.ts
+++ src/app/main/shared/timestamp.func.ts
@@ -7,9 +7,13 @@ export const getTimestampFromMoment = (m: any): number => {
   return parseInt(timestamp, 10);
 };
 
+export const getCurrentDate = (): string => moment.tz(TIMEZONE).format('LLL');
+
 export const getTimestampFromDate = (date: string): number => getTimestampFromMoment(moment(date, DATE_FORMAT));
 
 export const getCurrentTimestamp = (): number => getTimestampFromMoment(moment());
+
+export const getDateFormat = (dateStr: string): string => moment(dateStr).format(DATE_FORMAT);
 
 export const addDays = (timestamp: number, days: number): number => timestamp + days * DAY_SECONDS;
 export const addMinutes = (timestamp: number, minutes: number): number => timestamp + minutes * MINUTE_SECONDS;
